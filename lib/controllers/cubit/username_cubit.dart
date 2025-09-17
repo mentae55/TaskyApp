b70f5c79c58955883ed9c7edfe2d338cbb7f02f9
menna <mena.seaad23@gmail.com>
2025-09-18 00:38:26 +0300

    Delete lib/controllers directory

--- lib/controllers/cubit/username_cubit.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter_bloc/flutter_bloc.dart';
-
-class UsernameCubit extends Cubit<String> {
-  UsernameCubit() : super("");
-
-  void setUsername(String name) {
-    emit(name);
-  }
-}
